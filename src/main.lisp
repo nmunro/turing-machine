05f3e68f1c55c0cc1d19bf398e60e803a587fb31
Neil Munro <neilmunro@gmail.com>
2019-12-09 22:17:15 +0000

    Changed op-codes and fixed unit tests

--- src/main.lisp
+++ src/main.lisp
@@ -3,50 +3,78 @@
   (:export #:werk))
 (in-package :turing)
 
+;;;
+;; There's a bug, one cannot set indexes beyond the length of the program
+;; list must be resizable and populated with empty values
+;;;
+
 (defun werk (stream)
   ; Main loop, define op-codes and functions
   (labels
-      ((math (f program stack-pointer)
+      ((calc (f program stack-pointer)
         (let ((operand1 (nth (nth (+ 1 stack-pointer) program) program))
               (operand2 (nth (nth (+ 2 stack-pointer) program) program))
               (operand3 (nth (+ 3 stack-pointer) program)))
           (setf (nth operand3 program) (funcall f operand1 operand2))
-          program))
+          (values program (+ 4 stack-pointer))))
+
+       (jump (program stack-pointer)
+         (values program (nth (1+ stack-pointer) program)))
+
+       (jump-if (program stack-pointer)
+         0)
+
+       (jump-nif (program stack-pointer)
+         0)
 
        (equals (program stack-pointer)
          (let ((operand1 (nth (nth (+ 1 stack-pointer) program) program))
                (operand2 (nth (nth (+ 2 stack-pointer) program) program))
                (operand3 (nth (+ 3 stack-pointer) program)))
            (setf (nth operand3 program) (if (= operand1 operand2) 1 0))
-           program))
-
-       (if-else (program stack-pointer)
-         (let* ((program  (equals program stack-pointer))
-                (result   (nth (nth (+ 3 stack-pointer) program) program))
-                (branch-1 (nth (nth (+ 4 stack-pointer) program) program))
-                (branch-2 (nth (nth (+ 5 stack-pointer) program) program)))
-           (values program (if result branch-1 branch-2))))
+           (values program (+ 4 stack-pointer))))
 
        (run-instruction (program &optional (stack-pointer 0))
-         (format t "~A~%" stack-pointer)
          (cond
-           ; Halt, returning the state of the program
-           ((= 99 (nth stack-pointer program)) program)
+           ;; Halt, returning the state of the program
+           ((= 0 (nth stack-pointer program))
+            program)
 
-           ; Math operations
-           ((= 1  (nth stack-pointer program)) (run-instruction (math #'+ program stack-pointer) (+ stack-pointer 4)))
-           ((= 2  (nth stack-pointer program)) (run-instruction (math #'* program stack-pointer) (+ stack-pointer 4)))
-           ((= 3  (nth stack-pointer program)) (run-instruction (math #'- program stack-pointer) (+ stack-pointer 4)))
-           ((= 4  (nth stack-pointer program)) (run-instruction (math #'/ program stack-pointer) (+ stack-pointer 4)))
+           ;; Add
+           ((= 101 (nth stack-pointer program))
+            (multiple-value-bind (program stack-pointer)
+                                 (calc #'+ program stack-pointer)
+                                 (run-instruction program stack-pointer)))
 
-           ;  Program flow
-           ((= 5  (nth stack-pointer program)) (run-instruction program (nth (1+ stack-pointer) program)))
-           ((= 55 (nth stack-pointer program)) (multiple-value-bind (program stack-pointer)
-                                                                    (if-else program (nth (1+ stack-pointer) program))
-                                                 (run-instruction program stack-pointer)))
+           ;; Multiply
+           ((= 102 (nth stack-pointer program))
+            (multiple-value-bind (program stack-pointer)
+                                 (calc #'* program stack-pointer)
+                                 (run-instruction program stack-pointer)))
+
+           ;; Subtract
+           ((= 103 (nth stack-pointer program))
+            (multiple-value-bind (program stack-pointer)
+                                 (calc #'- program stack-pointer)
+                                 (run-instruction program stack-pointer)))
+
+           ;; Divide
+           ((= 104 (nth stack-pointer program))
+            (multiple-value-bind (program stack-pointer)
+                                 (calc #'/ program stack-pointer)
+                                 (run-instruction program stack-pointer)))
+
+           ;;  Program flow
+           ((= 201  (nth stack-pointer program))
+            (multiple-value-bind (program stack-pointer)
+                                 (jump program stack-pointer)
+                                 (run-instruction program stack-pointer)))
 
            ; Equality
-           ((= 6 (nth stack-pointer program)) (run-instruction (equals program stack-pointer) (+ stack-pointer 4)))
+           ((= 301 (nth stack-pointer program))
+            (multiple-value-bind (program stack-pointer)
+                                 (equals program stack-pointer)
+                                 (run-instruction program stack-pointer)))
 
            ; Errors
            (T (format nil "~A program error" (nth stack-pointer program))))))
@@ -54,4 +82,4 @@
     (let ((program (mapcar #'parse-integer (uiop:split-string stream :separator ","))))
       (run-instruction program))))
 
-(werk "55,15,0,13,6,10,1,0,0,16,2,0,0,16,99,55,0")
+(werk "201,7,6,2,4,1,0,104,2,3,4,0")
--- tests/main.lisp
+++ tests/main.lisp
@@ -7,50 +7,49 @@
 ;; NOTE: To run this test file, execute `(asdf:test-system :turing)' in your Lisp.
 
 (deftest test-add-halt-1
-  (testing "should (equal '(2 0 0 0 99) (werk \"1,0,0,0,99\")) to be true"
-    (ok (equal '(2 0 0 0 99) (werk "1,0,0,0,99")))))
+  (testing "should (equal '(202 0 0 0 0) (werk \"101,0,0,0,0\")) to be true"
+    (ok (equal '(202 0 0 0 0) (werk "101,0,0,0,0")))))
 
 (deftest test-add-halt-2
-  (testing "should (equal '(2 3 0 6 99) (werk \"2,3,0,3,99\")) to be true"
-    (ok (equal '(2 3 0 6 99) (werk "2,3,0,3,99")))))
+  (testing "should (equal '(102 3 0 6 0) (werk \"102,3,0,3,0\")) to be true"
+    (ok (equal '(102 3 0 306 0) (werk "102,3,0,3,0")))))
 
 (deftest test-add-halt-3
-  (testing "should (equal '(3 5 6 0 99 5 -2) (werk \"1,5,6,0,99,5,-2\")) to be true"
-    (ok (equal '(3 5 6 0 99 5 -2) (werk "1,5,6,0,99,5,-2")))))
+  (testing "should (equal '(3 5 6 0 0 5 -2) (werk \"101,5,6,0,0,5,-2\")) to be true"
+    (ok (equal '(3 5 6 0 0 5 -2) (werk "101,5,6,0,0,5,-2")))))
 
 (deftest test-multiply-halt-1
-  (testing "should (equal '(2 4 4 5 99 9801) (werk \"2,4,4,5,99,0\")) to be true"
-    (ok (equal '(2 4 4 5 99 9801) (werk "2,4,4,5,99,0")))))
+  (testing "should (equal '(102 5 5 6 0 9801) (werk \"102,5,5,6,0,9801\")) to be true"
+    (ok (equal '(102 9801 5 1 0 99) (werk "102,5,5,1,0,99")))))
 
 (deftest test-multiply-halt-2
-  (testing "should (equal '(30 1 1 4 2 5 6 0 99) (werk \"1,1,1,4,99,5,6,0,99\")) to be true"
-    (ok (equal '(30 1 1 4 2 5 6 0 99) (werk "1,1,1,4,99,5,6,0,99")))))
+  (testing "should (equal '(30 1 1 4 2 5 6 0 0) (werk \"1,1,1,4,0,5,6,0,0\")) to be true"
+    (ok (equal '(30 1 9 4 102 5 6 0 0 101) (werk "101,1,9,4,0,5,6,0,0,101")))))
 
 (deftest test-add-multiply-halt-1
-  (testing "should (equal '(3500 9 10 70 2 3 11 0 99 30 40 50) (werk \"1,9,10,3,2,3,11,0,99,30,40,50\")) to be true"
-    (ok (equal '(3500 9 10 70 2 3 11 0 99 30 40 50) (werk "1,9,10,3,2,3,11,0,99,30,40,50")))))
+  (testing "should (equal '(3500 9 10 70 2 3 11 0 0 30 40 50) (werk \"1,9,10,3,2,3,11,0,0,30,40,50\")) to be true"
+    (ok (equal '(3500 9 10 70 102 3 11 0 0 30 40 50) (werk "101,9,10,3,102,3,11,0,0,30,40,50")))))
 
 (deftest test-subtract-halt-1
-  (testing "should (equal '(8 5 6 0 99 10 2) (werk \"3,5,6,0,99,10,2\")) to be true"
-    (ok (equal '(8 5 6 0 99 10 2) (werk "3,5,6,0,99,10,2")))))
+  (testing "should (equal '(8 5 6 0 0 10 2) (werk \"3,5,6,0,0,10,2\")) to be true"
+    (ok (equal '(8 5 6 0 0 10 2) (werk "103,5,6,0,0,10,2")))))
 
 (deftest test-divide-halt-1
-  (testing "should (equal '(5 5 6 0 99 10 2) (werk \"4,5,6,0,99,10,2\")) to be true"
-    (ok (equal '(5 5 6 0 99 10 2) (werk "4,5,6,0,99,10,2")))))
+  (testing "should (equal '(5 5 6 0 0 10 2) (werk \"4,5,6,0,0,10,2\")) to be true"
+    (ok (equal '(5 5 6 0 0 10 2) (werk "104,5,6,0,0,10,2")))))
 
 (deftest test-jump-1
-  (testing "should (equal '(5 10 0 0 1 0 0 1 99) (werk \"5,4,0,0,1,0,0,1,99\")) to be true"
-    (ok (equal '(5 10 0 0 1 0 0 1 99) (werk "5,4,0,0,1,0,0,1,99")))))
+  (testing "should (equal '(201 10 0 0 1 0 0 1 0) (werk \"201,4,0,0,1,0,0,1,0\")) to be true"
+    (ok (equal '(201 402 0 0 101 0 0 1 0) (werk "201,4,0,0,101,0,0,1,0")))))
 
 (deftest test-jump-2
-  (testing "should (equal '(5 10 1 0 0 1 99 5 2) (werk \"5,7,1,0,0,1,99,5,2\")) to be true"
-    (ok (equal '(5 10 1 0 0 1 99 5 2) (werk "5,7,1,0,0,1,99,5,2")))))
+  (testing "should (equal '(201 10 1 0 0 1 0 5 2) (werk \"201,7,1,0,0,1,0,104,2\")) to be true"
+    (ok (equal '(201 7 6 2 3 1 0 104 2 3 4 0) (werk "201,7,6,2,4,1,0,104,2,3,4,0")))))
 
 (deftest test-equality-1
-  (testing "should (equal '(6 1 1 5 99 1) (werk \"6,1,1,5,99,0\")) to be true"
-    (ok (equal '(6 1 1 5 99 1) (werk "6,1,1,5,99,0")))))
+  (testing "should (equal '(301 1 1 5 0 1) (werk \"301,1,1,5,0,0\")) to be true"
+    (ok (equal '(301 1 1 5 0 1) (werk "301,1,1,5,0,0")))))
 
-(deftest test-if-else-1
-  (testing "should (equal '(99) (werk \"55,12,0,13,6,10,1,0,0,16,2,0,0,16,99,55,0\")) to be true"
-
-    (ok (equal '(99) (werk "99")))))
+;(deftest test-if-else-1
+;  (testing "should (equal '(0) (werk \"55,12,0,13,6,10,1,0,0,16,2,0,0,16,0,55,0\")) to be true"
+;    (ok (equal '(0) (werk "55,12,0,13,6,10,1,0,0,16,2,0,0,16,0,55,0")))))
